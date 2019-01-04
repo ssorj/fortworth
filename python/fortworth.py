5fc2b4de6947117b9cff784eb7d7fabb5addd80e
Justin Ross <jross@apache.org>
2019-01-04 09:32:48 -0800

    WIP

--- python/fortworth.py
+++ python/fortworth.py
@@ -18,6 +18,18 @@ skip_if_unavailable=1
 # sudo curl {yum_repo_url}/config.txt -o /etc/yum.repos.d/{repo}.repo
 """
 
+_global_maven_options = "-B -q"
+
+class BuildData(object):
+    def __init__(self, repo, branch, id, url=None):
+        assert repo
+        assert branch
+
+        self.repo = repo
+        self.branch = branch
+        self.id = id
+        self.url = url
+
 def git_current_commit(checkout_dir):
     with working_dir(checkout_dir):
         return call_for_stdout("git rev-parse HEAD").strip()
@@ -53,46 +65,33 @@ def stagger_put_artifact(repo, branch, tag, artifact, artifact_data):
     url = "{0}/api/repos/{1}/branches/{2}/tags/{3}/artifacts/{4}".format(_tag_service_url, repo, branch, tag, artifact)
     return http_put_json(url, artifact_data)
 
+def build_is_stored(build_data):
+    url = _files_url(build_data)
+
+    try:
+        http_get(url)
+    except CalledProcessError as e:
+        return False
+
+    return True
+
 # Requires /root/.ssh/id_rsa_file_service be present inside the container
-def store_build(build_dir, repo, branch, build_id):
+def store_build(build_dir, build_data):
     options = "-o LogLevel=ERROR -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /root/.ssh/id_rsa_file_service"
-    remote_dir = join("web", "builds", repo, branch, build_id)
+    remote_dir = join("web", "builds", build_data.repo, build_data.branch, build_data.id)
 
     call("ssh {0} -p {1} app@{2} 'rm -rf {3}; mkdir -p {4}'",
          options, _file_service_ssh_port, _file_service_ssh_host, remote_dir, parent_dir(remote_dir))
     call("scp {0} -P {1} -r {2} app@{3}:{4}",
          options, _file_service_ssh_port, build_dir, _file_service_ssh_host, remote_dir)
 
-def rpm_make_yum_repo_config(repo, branch, build_id):
-    yum_repo_url = _yum_repo_url(repo, branch, build_id)
+def rpm_make_yum_repo_config(build_data):
+    repo = build_data.repo
+    branch = build_data.branch
+    build_id = build_data.id
+    yum_repo_url = _yum_repo_url(build_data)
+
     return _yum_repo_config_template.lstrip().format(**locals())
-
-def rpm_make_tag_data(spec_file, source_dir, repo, branch, build_id, build_url=None):
-    records = call_for_stdout("rpm -q --qf '%{{name}},%{{version}},%{{release}}\n' --specfile {0}", spec_file)
-    artifacts = dict()
-
-    for record in records.split():
-        name, version, release = record.split(",")
-
-        artifact = {
-            "type": "rpm",
-            "repository_url": _yum_repo_url(repo, branch, build_id),
-            "name": name,
-            "version": version,
-            "release": release,
-        }
-
-        artifacts[name] = artifact
-
-    tag = {
-        "build_id": build_id,
-        "build_url": build_url,
-        "commit_id": git_current_commit(source_dir),
-        "files_url": _files_url(repo, branch, build_id),
-        "artifacts": artifacts,
-    }
-
-    return tag
 
 def rpm_install_tag_packages(repo, branch, tag, *packages):
     tag_data = stagger_get_tag(repo, branch, tag)
@@ -104,9 +103,11 @@ def rpm_install_tag_packages(repo, branch, tag, *packages):
         http_get(url, output_file="/etc/yum.repos.d/{0}.repo".format(repo))
         call("yum -y install {0}", package)
 
-def rpm_configure(input_spec_file, output_spec_file, source_dir, build_id):
+def rpm_configure(input_spec_file, output_spec_file, source_dir, build_data):
     assert input_spec_file.endswith(".in"), input_spec_file
     assert is_dir(join(source_dir, ".git"))
+
+    build_id = build_data.id
 
     if build_id is None:
         build_id = 0
@@ -116,13 +117,13 @@ def rpm_configure(input_spec_file, output_spec_file, source_dir, build_id):
 
     configure_file(input_spec_file, output_spec_file, release=release)
 
-def rpm_build(spec_file, source_dir, build_dir, repo, branch, tag, build_id, build_url=None):
+def rpm_build(spec_file, source_dir, build_dir, build_data):
     records = call_for_stdout("rpm -q --qf '%{{name}}-%{{version}}\n' --specfile {0}", spec_file)
     archive_stem = records.split()[0]
     rpms_dir = join(build_dir, "RPMS")
     output_dir = make_temp_dir()
     yum_repo_dir = join(output_dir, "repo")
-    yum_repo_config = rpm_make_yum_repo_config(repo, branch, build_id)
+    yum_repo_config = rpm_make_yum_repo_config(build_data)
     yum_repo_file = join(yum_repo_dir, "config.txt")
 
     git_make_archive(source_dir, join(build_dir, "SOURCES"), archive_stem)
@@ -131,62 +132,57 @@ def rpm_build(spec_file, source_dir, build_dir, repo, branch, tag, build_id, bui
     call("createrepo {0}", yum_repo_dir)
     write(yum_repo_file, yum_repo_config)
 
-    tag_data = rpm_make_tag_data(spec_file, source_dir, repo, branch, build_id, build_url=build_url)
-
-    if build_id is not None:
-        store_build(output_dir, repo, branch, build_id)
-        stagger_put_tag(repo, branch, tag, tag_data)
-
-def rpm_publish(spec_file, source_dir, repo, branch, tag, build_id, build_url=None):
-    tag_data = rpm_make_tag_data(spec_file, source_dir, repo, branch, build_id, build_url=build_url)
-
+def rpm_publish(spec_file, source_dir, build_dir, build_data, tag):
     # Skip developer test builds
-    if build_id is not None:
-        stagger_put_tag(repo, branch, tag, tag_data)
+    if build_data.id is None:
+        return
 
-def maven_make_tag_data(source_dir, build_dir, repo, branch, build_id, build_url=None):
-    maven_repo_dir = absolute_path(join(build_dir, "repo"))
+    if not build_is_stored(build_data):
+        store_build(build_dir, build_data)
+
+    tag_data = _rpm_make_tag_data(spec_file, source_dir, build_data)
+
+    stagger_put_tag(build_data.repo, build_data.branch, tag, tag_data)
+
+def _rpm_make_tag_data(spec_file, source_dir, build_data):
+    records = call_for_stdout("rpm -q --qf '%{{name}},%{{version}},%{{release}}\n' --specfile {0}", spec_file)
     artifacts = dict()
 
-    with working_dir(source_dir):
-        records = call_for_stdout("mvn -q -Dmaven.repo.local={0} -Dexec.executable=echo -Dexec.args='${{project.groupId}},${{project.artifactId}},${{project.version}}' exec:exec", maven_repo_dir)
-
     for record in records.split():
-        group_id, artifact_id, version = record.split(",")
+        name, version, release = record.split(",")
 
         artifact = {
-            "type": "maven",
-            "repository_url": _maven_repo_url(repo, branch, build_id),
-            "group_id": group_id,
-            "artifact_id": artifact_id,
+            "type": "rpm",
+            "repository_url": _yum_repo_url(build_data),
+            "name": name,
             "version": version,
+            "release": release,
         }
 
-        artifacts[artifact_id] = artifact
+        artifacts[name] = artifact
 
-    data = {
-        "build_id": build_id,
-        "build_url": build_url,
+    tag = {
+        "build_id": build_data.id,
+        "build_url": build_data.url,
         "commit_id": git_current_commit(source_dir),
-        "files_url": _files_url(repo, branch, build_id),
         "artifacts": artifacts,
     }
 
-    return data
+    return tag
 
 # mvn versions:use-dep-version -Dincludes=junit:junit -DdepVersion=1.0 -DforceVersion=true
 
-def maven_build(source_dir, build_dir, repo, branch, tag, build_id, repo_urls=[], properties={}, build_url=None):
+def maven_build(source_dir, build_dir, build_data, repo_urls=[], properties={}):
     maven_repo_dir = absolute_path(join(build_dir, "repo"))
     settings_file = _make_settings_file(repo_urls)
 
     with working_dir(source_dir):
         commit_id = git_current_commit(".")
-        version = call_for_stdout("mvn -q -Dexec.executable=echo -Dexec.args='${{project.version}}' --non-recursive exec:exec")
+        version = call_for_stdout("mvn {0} -Dexec.executable=echo -Dexec.args='${{project.version}}' --non-recursive exec:exec", _global_maven_options)
         version = version.strip()
-        version = version.replace("SNAPSHOT", "{0}.{1}".format(build_id, commit_id[:8]))
+        version = version.replace("SNAPSHOT", "{0}.{1}".format(build_data.id, commit_id[:8]))
 
-        call("mvn versions:set -DnewVersion={0}", version)
+        call("mvn {0} -DnewVersion={1} versions:set", _global_maven_options, version)
 
         options = [
             "-U",
@@ -198,14 +194,7 @@ def maven_build(source_dir, build_dir, repo, branch, tag, build_id, repo_urls=[]
         for name, value in properties.items():
             options.append("-D{0}={1}".format(name, value))
 
-        call("mvn {0} install", " ".join(options))
-
-    tag_data = maven_make_tag_data(source_dir, build_dir, repo, branch, build_id, build_url=build_url)
-
-    # Skip developer test builds
-    if build_id is not None:
-        store_build(build_dir, repo, branch, build_id)
-        stagger_put_tag(repo, branch, tag, tag_data)
+        call("mvn {0} {1} install", _global_maven_options, " ".join(options))
 
 def _make_settings_file(repo_urls):
     repos = list()
@@ -235,18 +224,54 @@ def _make_settings_file(repo_urls):
 
     return write(make_temp_file(), xml)
 
-def maven_publish(source_dir, build_dir, repo, branch, tag, build_id, build_url=None):
-    tag_data = maven_make_tag_data(source_dir, build_dir, repo, branch, build_id, build_url=build_url)
-
+def maven_publish(source_dir, build_dir, build_data, tag):
     # Skip developer test builds
-    if build_id is not None:
-        stagger_put_tag(repo, branch, tag, tag_data)
+    if build_data.id is None:
+        return
 
-def _files_url(repo, branch, build_id):
-    return "{0}/builds/{1}/{2}/{3}".format(_file_service_url, repo, branch, build_id)
+    if not build_is_stored(build_data):
+        store_build(build_dir, build_data)
 
-def _yum_repo_url(repo, branch, build_id):
-    return "{0}/repo".format(_files_url(repo, branch, build_id))
+    tag_data = _maven_make_tag_data(source_dir, build_dir, build_data)
 
-def _maven_repo_url(repo, branch, build_id):
-    return "{0}/repo".format(_files_url(repo, branch, build_id))
+    stagger_put_tag(build_data.repo, build_data.branch, tag, tag_data)
+
+def _maven_make_tag_data(source_dir, build_dir, build_data):
+    maven_repo_dir = absolute_path(join(build_dir, "repo"))
+    artifacts = dict()
+
+    with working_dir(source_dir):
+        records = call_for_stdout("mvn {0} -Dmaven.repo.local={1} -Dexec.executable=echo -Dexec.args='${{project.groupId}},${{project.artifactId}},${{project.version}}' exec:exec",
+                                  _global_maven_options, maven_repo_dir)
+
+    for record in records.split():
+        group_id, artifact_id, version = record.split(",")
+
+        artifact = {
+            "type": "maven",
+            "repository_url": _maven_repo_url(build_data),
+            "group_id": group_id,
+            "artifact_id": artifact_id,
+            "version": version,
+        }
+
+        artifacts[artifact_id] = artifact
+
+    data = {
+        "build_id": build_data.id,
+        "build_url": build_data.url,
+        "commit_id": git_current_commit(source_dir),
+        "artifacts": artifacts,
+    }
+
+    return data
+
+def _files_url(build_data):
+    return "{0}/builds/{1}/{2}/{3}".format \
+        (_file_service_url, build_data.repo, build_data.branch, build_data.id)
+
+def _yum_repo_url(build_data):
+    return "{0}/repo".format(_files_url(build_data))
+
+def _maven_repo_url(build_data):
+    return "{0}/repo".format(_files_url(build_data))
