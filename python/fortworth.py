19cefb26dc8442a15bb201c4c86cd242b5c2ad6c
Justin Ross <jross@apache.org>
2019-06-17 09:17:51 -0400

    Some renaming

--- python/fortworth.py
+++ python/fortworth.py
@@ -20,7 +20,7 @@ skip_if_unavailable=1
 
 _global_maven_options = "-B -q"
 
-class BuildData(object):
+class BuildInfo(object):
     def __init__(self, repo, branch, id, url=None):
         assert repo
         assert branch
@@ -30,17 +30,19 @@ class BuildData(object):
         self.id = id
         self.url = url
 
-def git_current_source_url(checkout_dir):
+def git_get_source_url(checkout_dir):
     with working_dir(checkout_dir):
         return call_for_stdout("git config --get remote.origin.url").strip()
 
-def git_current_commit(checkout_dir):
+def git_get_commit(checkout_dir):
     with working_dir(checkout_dir):
         return call_for_stdout("git rev-parse HEAD").strip()
 
-def git_current_commit_url(checkout_dir):
-    source_url = git_current_source_url(checkout_dir)
-    commit = git_current_commit(checkout_dir)
+def git_get_commit_url(checkout_dir, commit=None):
+    source_url = git_get_source_url(checkout_dir)
+
+    if commit is None:
+        commit = git_get_commit(checkout_dir)
 
     if source_url.startswith("https://github.com/"):
         try:
@@ -53,7 +55,7 @@ def git_current_commit_url(checkout_dir):
 
         return "https://github.com/{0}/{1}/commit/{2}".format(party, repo, commit)
 
-def git_current_branch(checkout_dir):
+def git_get_branch(checkout_dir):
     with working_dir(checkout_dir):
         return call_for_stdout("git rev-parse --abbrev-ref HEAD").strip()
 
@@ -124,8 +126,8 @@ def stagger_put_artifact(repo, branch, tag, artifact, artifact_data, service_url
 
     return response.text
 
-def bodega_put_build(build_dir, build_data, service_url=_bodega_url):
-    build_url = bodega_build_url(build_data, service_url=service_url)
+def bodega_put_build(build_dir, build_info, service_url=_bodega_url):
+    build_url = bodega_build_url(build_info, service_url=service_url)
     session = _requests.Session()
 
     for fs_path in find(build_dir):
@@ -135,29 +137,29 @@ def bodega_put_build(build_dir, build_data, service_url=_bodega_url):
         relative_path = fs_path[len(build_dir) + 1:]
         request_url = "{0}/{1}".format(build_url, relative_path)
 
-        if build_data.id is None:
+        if build_info.id is None:
             request_url += "?dry-run=1"
 
         with open(fs_path, "rb") as f:
             response = session.put(request_url, data=f)
             response.raise_for_status()
 
-def bodega_build_exists(build_data, service_url=_bodega_url):
-    build_url = bodega_build_url(build_data, service_url=service_url)
+def bodega_build_exists(build_info, service_url=_bodega_url):
+    build_url = bodega_build_url(build_info, service_url=service_url)
 
     response = _requests.get(build_url)
 
     return response.status_code == _requests.codes.ok
 
-def bodega_build_url(build_data, service_url=_bodega_url):
+def bodega_build_url(build_info, service_url=_bodega_url):
     assert service_url
-    return "{0}/{1}/{2}/{3}".format(service_url, build_data.repo, build_data.branch, build_data.id)
+    return "{0}/{1}/{2}/{3}".format(service_url, build_info.repo, build_info.branch, build_info.id)
 
-def rpm_make_yum_repo_config(build_data):
-    repo = build_data.repo
-    branch = build_data.branch
-    build_id = build_data.id
-    yum_repo_url = _yum_repo_url(build_data)
+def rpm_make_yum_repo_config(build_info):
+    repo = build_info.repo
+    branch = build_info.branch
+    build_id = build_info.id
+    yum_repo_url = _yum_repo_url(build_info)
 
     return _yum_repo_config_template.lstrip().format(**locals())
 
@@ -186,13 +188,13 @@ def rpm_configure(input_spec_file, output_spec_file, source_dir, build_id, **sub
 
     configure_file(input_spec_file, output_spec_file, release=release, **substitutions)
 
-def rpm_build(spec_file, source_dir, build_dir, build_data):
+def rpm_build(spec_file, source_dir, build_dir, build_info):
     records = call_for_stdout("rpm -q --qf '%{{name}}-%{{version}}\n' --specfile {0}", spec_file)
     archive_stem = records.split()[0]
     rpms_dir = join(build_dir, "RPMS")
     dist_dir = make_dir(join(build_dir, "dist"))
     yum_repo_dir = join(dist_dir, "repo")
-    yum_repo_config = rpm_make_yum_repo_config(build_data)
+    yum_repo_config = rpm_make_yum_repo_config(build_info)
     yum_repo_file = join(yum_repo_dir, "config.txt")
 
     git_make_archive(source_dir, join(build_dir, "SOURCES"), archive_stem)
@@ -201,19 +203,19 @@ def rpm_build(spec_file, source_dir, build_dir, build_data):
     call("createrepo {0}", yum_repo_dir)
     write(yum_repo_file, yum_repo_config)
 
-def rpm_publish(spec_file, source_dir, build_dir, build_data, tag):
+def rpm_publish(spec_file, source_dir, build_dir, build_info, tag):
     # Skip developer test builds
-    if build_data.id is None:
+    if build_info.id is None:
         return
 
-    if not bodega_build_exists(build_data):
-        bodega_put_build(join(build_dir, "dist"), build_data)
+    if not bodega_build_exists(build_info):
+        bodega_put_build(join(build_dir, "dist"), build_info)
 
-    tag_data = _rpm_make_tag_data(spec_file, source_dir, build_data)
+    tag_data = _rpm_make_tag_data(spec_file, source_dir, build_info)
 
-    stagger_put_tag(build_data.repo, build_data.branch, tag, tag_data)
+    stagger_put_tag(build_info.repo, build_info.branch, tag, tag_data)
 
-def _rpm_make_tag_data(spec_file, source_dir, build_data):
+def _rpm_make_tag_data(spec_file, source_dir, build_info):
     records = call_for_stdout("rpm -q --qf '%{{name}},%{{version}},%{{release}}\n' --specfile {0}", spec_file)
     artifacts = dict()
 
@@ -222,7 +224,7 @@ def _rpm_make_tag_data(spec_file, source_dir, build_data):
 
         artifact = {
             "type": "rpm",
-            "repository_url": _yum_repo_url(build_data),
+            "repository_url": _yum_repo_url(build_info),
             "name": name,
             "version": version,
             "release": release,
@@ -231,8 +233,8 @@ def _rpm_make_tag_data(spec_file, source_dir, build_data):
         artifacts[name] = artifact
 
     tag = {
-        "build_id": build_data.id,
-        "build_url": build_data.url,
+        "build_id": build_info.id,
+        "build_url": build_info.url,
         "commit_id": git_current_commit(source_dir),
         "commit_url": git_current_commit_url(source_dir),
         "artifacts": artifacts,
@@ -242,7 +244,7 @@ def _rpm_make_tag_data(spec_file, source_dir, build_data):
 
 # mvn versions:use-dep-version -Dincludes=junit:junit -DdepVersion=1.0 -DforceVersion=true
 
-def maven_build(source_dir, build_dir, build_data, repo_urls=[], properties={}):
+def maven_build(source_dir, build_dir, build_info, repo_urls=[], properties={}):
     maven_repo_dir = get_absolute_path(join(build_dir, "repo"))
     settings_file = _make_settings_file(repo_urls)
 
@@ -250,7 +252,7 @@ def maven_build(source_dir, build_dir, build_data, repo_urls=[], properties={}):
         commit_id = git_current_commit(".")
         version = call_for_stdout("mvn {0} -Dexec.executable=echo -Dexec.args='${{project.version}}' --non-recursive exec:exec", _global_maven_options)
         version = version.strip()
-        version = version.replace("SNAPSHOT", "{0}.{1}".format(build_data.id, commit_id[:8]))
+        version = version.replace("SNAPSHOT", "{0}.{1}".format(build_info.id, commit_id[:8]))
 
         call("mvn {0} -DnewVersion={1} versions:set", _global_maven_options, version)
 
@@ -294,19 +296,19 @@ def _make_settings_file(repo_urls):
 
     return write(make_temp_file(), xml)
 
-def maven_publish(source_dir, build_dir, build_data, tag):
+def maven_publish(source_dir, build_dir, build_info, tag):
     # Skip developer test builds
-    if build_data.id is None:
+    if build_info.id is None:
         return
 
-    if not bodega_build_exists(build_data):
-        bodega_put_build(build_dir, build_data)
+    if not bodega_build_exists(build_info):
+        bodega_put_build(build_dir, build_info)
 
-    tag_data = _maven_make_tag_data(source_dir, build_dir, build_data)
+    tag_data = _maven_make_tag_data(source_dir, build_dir, build_info)
 
-    stagger_put_tag(build_data.repo, build_data.branch, tag, tag_data)
+    stagger_put_tag(build_info.repo, build_info.branch, tag, tag_data)
 
-def _maven_make_tag_data(source_dir, build_dir, build_data):
+def _maven_make_tag_data(source_dir, build_dir, build_info):
     maven_repo_dir = get_absolute_path(join(build_dir, "repo"))
     artifacts = dict()
 
@@ -319,7 +321,7 @@ def _maven_make_tag_data(source_dir, build_dir, build_data):
 
         artifact = {
             "type": "maven",
-            "repository_url": _maven_repo_url(build_data),
+            "repository_url": _maven_repo_url(build_info),
             "group_id": group_id,
             "artifact_id": artifact_id,
             "version": version,
@@ -328,8 +330,8 @@ def _maven_make_tag_data(source_dir, build_dir, build_data):
         artifacts[artifact_id] = artifact
 
     data = {
-        "build_id": build_data.id,
-        "build_url": build_data.url,
+        "build_id": build_info.id,
+        "build_url": build_info.url,
         "commit_id": git_current_commit(source_dir),
         "commit_url": git_current_commit_url(source_dir),
         "artifacts": artifacts,
@@ -337,10 +339,10 @@ def _maven_make_tag_data(source_dir, build_dir, build_data):
 
     return data
 
-def _yum_repo_url(build_data, service_url=_bodega_url):
+def _yum_repo_url(build_info, service_url=_bodega_url):
     assert service_url
-    return "{0}/repo".format(bodega_build_url(build_data, service_url=service_url))
+    return "{0}/repo".format(bodega_build_url(build_info, service_url=service_url))
 
-def _maven_repo_url(build_data, service_url=_bodega_url):
+def _maven_repo_url(build_info, service_url=_bodega_url):
     assert service_url
-    return "{0}/repo".format(bodega_build_url(build_data, service_url=service_url))
+    return "{0}/repo".format(bodega_build_url(build_info, service_url=service_url))
